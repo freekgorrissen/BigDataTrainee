ec9219b55db74a67a28e91a009de7df476e92371
freekgorrissen <f.gorrissen@gmail.com>
2021-10-26 16:57:34 +0200

    sterren dataset toegevoegd met coordinaten

---
+++ AsteroidMining/pdfscrape.py
@@ -0,0 +1,11 @@
+import pds4_tools as pds4
+
+path = 'C:/Users/fgorr/OneDrive/Desktop/Traineeship/BigData/AsteroidMining/Asteroids/SMASSII/'
+file = 'bundle_gbo.ast.smass2.spectra.xml'
+
+pds4.pds4_read(path + file)
+# import camelot
+#
+# file = 'bus.pdf'
+#
+# camelot.read_pdf(file)
